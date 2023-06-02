e6a82fb3ed778a9c50c4f5adc818b37fb316a097
drod <drodriguez@bakerrisk.com>
2023-06-02 15:09:44 -0500

    Docstring for function

--- HelperFuncs.py
+++ HelperFuncs.py
@@ -1,3 +1,15 @@
+from typing import Tuple
+
 # Function to interploate on curve
-def InterpolateArray():
+def InterpolateArray(x: list, y:list) -> Tuple[float, float]:
+    """_summary_
+    
+    Args:
+        x (list): _description_
+        y (list): _description_
+    """
     pass
+    x = 1
+    y = 1
+
+    return (x, y)
