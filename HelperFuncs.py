4728015906cea3cff4bac204f72612017d625ed5
Amandeep Sharma <asharma@BakerRisk.com>
2023-06-02 16:09:30 -0500

    Solver Class

--- HelperFuncs.py
+++ HelperFuncs.py
@@ -1,15 +1,13 @@
 from typing import Tuple
+from scipy.interpolate import interp1d
 
 # Function to interploate on curve
-def InterpolateArray(x: list, y:list) -> Tuple[float, float]:
+def InterpolateArray(x: list, y:list, xValue: float) -> Tuple[float, float]:
     """_summary_
-    
+    Returns the interpolated array from the given x and y values    
     Args:
         x (list): _description_
         y (list): _description_
     """
-    pass
-    x = 1
-    y = 1
-
-    return (x, y)
+    f = interp1d(x, y, kind='linear')
+    return f(x)
--- Solver.py
+++ Solver.py
@@ -1,5 +1,30 @@
 from HelperFuncs import InterpolateArray
 import numpy as np
+from Inputs import inputs
+
+class Solver:
+    # Class Properties
+    MaxLevelInbound = None
+    MaxLevelOutBound = None
+
+    def __init__(self, inputs: dict) -> None:
+        self.inputs = inputs
+        self.nts = int(inputs["TT"] / inputs["dt"]) # Time Step
+        self.x = np.array(self.nts)
+        self.V = np.array(self.nts)
+        self.a = np.array(self.nts)
 
 
-InterpolateArray()
+    def InitializeStiffness(self):        
+        self.MaxLevelInbound =1
+        self.MaxLevelOutBound = 1
+
+
+        C = (self.inputs["C_in"] / 100) * 2 * abs(k * M) ** 0.5
+        pass
+
+    def GetMassFromRegion(self, region: int) -> float:
+        return self.inputs["M_in"][0][region]
+    
+    def GetStiffnessFromRegion(self, region: int) -> float:
+        return self.inputs["K_LM"][0][region]
